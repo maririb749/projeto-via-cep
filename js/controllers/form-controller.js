e19a2e2a4506e3c83c78d270eb2ab42e61912353
Mariana Santos <maririb51@gmail.com>
2023-04-25 14:50:07 +0100

    Criando o listController para inserir os cards com o novo endereço inserido na listagem.

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -1,6 +1,6 @@
 import Address from "../models/address.js";
 import * as addressService from '../services/address-service.js';
-
+import * as listController from './list-controller.js';
 
 function State() {
 
@@ -69,7 +69,7 @@ async function handleInputCepChange(event) {
 
 async function handleBtnSaveClick(event) {
     event.preventDefault();
-    console.log(state.address);
+    listController.addCCard(state.address);
 
 }
 
